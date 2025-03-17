ddb79c4ad835b86c20b34413ea83a8c3f81c8c20
Quinten <quinten.roets@gmail.com>
2025-03-16 22:40:51 -0500

    correctly exit

--- src/hostfinder/main/main.py
+++ src/hostfinder/main/main.py
@@ -11,5 +11,5 @@ def find_hosts(options: Options | None = None) -> list[str]:
 def find_host(options: Options | None = None) -> str | None:
     host_finder = HostFinder() if options is None else HostFinder(options)
     host_finder.options.show_progress = False
-    hosts = host_finder.find_hosts()
+    hosts = iter(list(host_finder.find_hosts()))
     return next(hosts, None)
