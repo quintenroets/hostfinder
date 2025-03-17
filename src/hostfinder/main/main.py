885c261ea9dd47010f2d81921174e78a803f7085
Quinten <quinten.roets@gmail.com>
2025-03-16 22:44:49 -0500

    cleanup exit

--- src/hostfinder/main/main.py
+++ src/hostfinder/main/main.py
@@ -1,4 +1,5 @@
 from hostfinder.models import Options
+import collections
 
 from .hostfinder import HostFinder
 
@@ -11,5 +12,7 @@ def find_hosts(options: Options | None = None) -> list[str]:
 def find_host(options: Options | None = None) -> str | None:
     host_finder = HostFinder() if options is None else HostFinder(options)
     host_finder.options.show_progress = False
-    hosts = iter(list(host_finder.find_hosts()))
-    return next(hosts, None)
+    hosts = host_finder.find_hosts()
+    host = next(hosts, None)
+    collections.deque(hosts, maxlen=0)
+    return host
